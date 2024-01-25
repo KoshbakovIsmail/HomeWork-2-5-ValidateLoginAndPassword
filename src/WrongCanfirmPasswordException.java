1dcd742a96783f4f99ac7e151268468eb7f8cce3
KosbakovIsmail <kosbakovismail@gmail.com>
2024-01-25 22:45:26 +0300

    Initial commit

---
+++ src/WrongCanfirmPasswordException.java
@@ -0,0 +1,6 @@
+public class WrongCanfirmPasswordException extends Exception{
+    public WrongCanfirmPasswordException(String e) {
+        super(e);
+    }
+}
+
---
+++ src/WrongLoginException.java
@@ -0,0 +1,5 @@
+public class WrongLoginException extends Exception{
+    public WrongLoginException(String e) {
+        super(e);
+    }
+}
